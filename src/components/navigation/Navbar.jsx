fe9ad8e1b9ea1e4c4997d427e35e3dfc19aaa244
Luka <lukalai1@orange.fr>
2022-12-23 15:56:24 +0100

    Favicon/Navbar

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,61 +1,7 @@
 import React, {useState} from "react";
 import "./Navbar.css";
-import {HiOutlineMenu} from 'react-icons/hi';
-import {IoClose} from 'react-icons/io5';
-import {Link} from 'react-router-dom';
-
-/*
-export default function Navbar() {
-    
-    const [toggleMenu, setToggleMenu] = useState(false);
-    const [largeur, setLargeur] = useState (window.innerWidth)
-    
-
-    const toggleNavSmallScreen = () => {
-        setToggleMenu(!toggleMenu);
-    }
-    const HamburgerIcon = <HiOutlineMenu className='btn' onClick={toggleNavSmallScreen}/>
-    const CloseIcon = <IoClose className='btn' onClick={toggleNavSmallScreen}/>
-
-    useEffect(() => {
-        const changeWidth = () => {
-            setLargeur(window.innerWidth); 
-        
-            if(window.innerWidth > 500){
-            setToggleMenu(false);
-            }
-        }
-
-        window.addEventListener('resize', changeWidth);
-
-        return () => {
-            window.removeEventListener('resize', changeWidth);
-        }
-
-    }, [])
-
-    return(
-        <nav>
-            {(toggleMenu || largeur > 500) && (
-                
-                <ul className="liste">
-                    <li className="items">
-                        <Link to="/"> 
-                            Accueil
-                        </Link>
-                    </li>
-                    <li className="items">
-                        <Link to="/contact">
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
-            )}
-            {toggleMenu ? CloseIcon : HamburgerIcon}  
-        </nav>
-    )
-}
-*/
+import logo from '../../assets/img/Logo-3cm-72.png';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     const[showLinks, setShowLinks] = useState(false)
@@ -65,15 +11,21 @@ function Navbar() {
     }
     return (
         <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
-            <div className="navbar_logo">Logo
-                <img src=""></img>
+            <div className="navbar_logo">
+                <Link to='/'>
+                    <img src={logo}></img>
+                </Link>
             </div>
             <ul className="navbar_links">
                 <li className="navbar_item">
-                    <a href="/" className="navbar_link">Accueil</a> 
+                    <Link to='/'>
+                    <a className="navbar_link">Accueil</a> 
+                    </Link>
                 </li>
                 <li className="navbar_item">
-                    <a href="/" className="navbar_link">Contact</a> 
+                    <Link to='/contact'>
+                        <a className="navbar_link">Contact</a> 
+                    </Link>
                 </li>
             </ul>
             <button className="navbar_burger" onClick={handleShowLinks}>
